e67faf3908b8411426b86dec3ca829322049a0fa
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2023-09-16 01:45:40 -0400

    Setting up auth

--- README.md
+++
@@ -1,6 +0,0 @@
-###
-VESMIA
-
-Explore unseen galaxies with us, and dive deeper into music than you ever have! 
-
-#sponsoredBySpotify
---
+++ public/javascripts/main.js
@@ -0,0 +1,8 @@
+// Testing file connection
+
+const testButton = document.getElementById("testButton");
+
+testButton.onclick = function() {
+  testButton.classList.toggle("blue")
+}
+
