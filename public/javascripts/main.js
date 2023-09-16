c694bb6c05139f2cc2995ab26812268c10d1a296
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2023-09-16 12:55:30 -0400

    ginal

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -102,32 +102,139 @@ for (let i = 0; i < 5; i++) {
 
 // add the layer to the stage
 
+// for loops are 4 losers
+// PLANET 1
+let i = 0;
+var imageObj = new Image();
+var planetLayer = new Konva.Layer();
+imageObj.onload = function () {
+  var img = new Konva.Image({
+    image: imageObj,
+    x: getXPointOnEllipse(getXRadius(i), getYRadius(i), Math.PI /(2)) - 100,
+    y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(i), getYRadius(i),  Math.PI /2),
+    width: 200,
+    height: 137,
+    draggable: true,
+    dragBoundFunc: function (pos) {
+      let theta;
+      if (pos.x > getXRadius(i)) theta = 0;
+      else theta =  Math.PI/2 * (1 - pos.x/getXRadius(i));
+      return {
+        x: getXPointOnEllipse(getXRadius(i), getYRadius(i), theta) - 100,
+        y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(i), getYRadius(i), theta),
+      };
+    },
+  });
+  planetLayer.add(img);
+  stage.add(planetLayer);
+};
+imageObj.src = `/images/planet_1.png`;
 
-for (let i = 0; i < 5; i++) {
-  var imageObj = new Image();
-  var planetLayer = new Konva.Layer();
-  imageObj.onload = function () {
-    var img = new Konva.Image({
-      image: imageObj,
-      x: getXPointOnEllipse(getXRadius(i), getYRadius(i), Math.PI /(2)) - 100,
-      y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(i), getYRadius(i),  Math.PI /2),
-      width: 200,
-      height: 137,
-      draggable: true,
-      dragBoundFunc: function (pos) {
-        let theta;
-        if (pos.x > getXRadius(i)) theta = 0;
-        else theta =  Math.PI/2 * (1 - pos.x/getXRadius(i));
-        return {
-          x: getXPointOnEllipse(getXRadius(i), getYRadius(i), theta) - 100,
-          y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(i), getYRadius(i), theta),
-        };
-      },
-    });
-    planetLayer.add(img);
-    stage.add(planetLayer);
-  };
-  imageObj.src = `/images/planet_${i+1}.png`;
-  
-}
+// PLANET 2
+let j = 1;
+var imageObj2 = new Image();
+var planetLayer = new Konva.Layer();
+imageObj2.onload = function () {
+  var img = new Konva.Image({
+    image: imageObj2,
+    x: getXPointOnEllipse(getXRadius(j), getYRadius(j), Math.PI /(2)) - 100,
+    y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(j), getYRadius(j),  Math.PI /2),
+    width: 200,
+    height: 137,
+    draggable: true,
+    dragBoundFunc: function (pos) {
+      let theta;
+      if (pos.x > getXRadius(j)) theta = 0;
+      else theta =  Math.PI/2 * (1 - pos.x/getXRadius(j));
+      return {
+        x: getXPointOnEllipse(getXRadius(j), getYRadius(j), theta) - 100,
+        y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(j), getYRadius(j), theta),
+      };
+    },
+  });
+  planetLayer.add(img);
+  stage.add(planetLayer);
+};
+imageObj2.src = `/images/planet_2.png`;
 
+
+// PLANET 3
+let k = 2;
+var imageObj3 = new Image();
+var planetLayer = new Konva.Layer();
+imageObj3.onload = function () {
+  var img = new Konva.Image({
+    image: imageObj3,
+    x: getXPointOnEllipse(getXRadius(k), getYRadius(k), Math.PI /(2)) - 100,
+    y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(k), getYRadius(k),  Math.PI /2),
+    width: 200,
+    height: 137,
+    draggable: true,
+    dragBoundFunc: function (pos) {
+      let theta;
+      if (pos.x > getXRadius(k)) theta = 0;
+      else theta =  Math.PI/2 * (1 - pos.x/getXRadius(k));
+      return {
+        x: getXPointOnEllipse(getXRadius(k), getYRadius(k), theta) - 100,
+        y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(k), getYRadius(k), theta),
+      };
+    },
+  });
+  planetLayer.add(img);
+  stage.add(planetLayer);
+};
+imageObj3.src = `/images/planet_3.png`;
+
+// PLANET 4
+let q = 3;
+var imageObj4 = new Image();
+var planetLayer = new Konva.Layer();
+imageObj4.onload = function () {
+  var img = new Konva.Image({
+    image: imageObj4,
+    x: getXPointOnEllipse(getXRadius(q), getYRadius(q), Math.PI /(2)) - 100,
+    y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(q), getYRadius(q),  Math.PI /2),
+    width: 200,
+    height: 137,
+    draggable: true,
+    dragBoundFunc: function (pos) {
+      let theta;
+      if (pos.x > getXRadius(q)) theta = 0;
+      else theta =  Math.PI/2 * (1 - pos.x/getXRadius(q));
+      return {
+        x: getXPointOnEllipse(getXRadius(q), getYRadius(q), theta) - 100,
+        y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(q), getYRadius(q), theta),
+      };
+    },
+  });
+  planetLayer.add(img);
+  stage.add(planetLayer);
+};
+imageObj4.src = `/images/planet_4.png`;
+
+// PLANET 5
+let h = 4;
+var imageObj5 = new Image();
+var planetLayer = new Konva.Layer();
+imageObj5.onload = function () {
+  var img = new Konva.Image({
+    image: imageObj5,
+    x: getXPointOnEllipse(getXRadius(h), getYRadius(h), Math.PI /(2)) - 100,
+    y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(h), getYRadius(h),  Math.PI /2),
+    width: 200,
+    height: 137,
+    draggable: true,
+    dragBoundFunc: function (pos) {
+      let theta;
+      if (pos.x > getXRadius(h)) theta = 0;
+      else theta =  Math.PI/2 * (1 - pos.x/getXRadius(q));
+      return {
+        x: getXPointOnEllipse(getXRadius(h), getYRadius(h), theta) - 100,
+        y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(h), getYRadius(h), theta),
+      };
+    },
+  });
+  planetLayer.add(img);
+  stage.add(planetLayer);
+};
+imageObj5.src = `/images/planet_5.png`;
