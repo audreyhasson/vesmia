285831d8de5b84f6ea086f6204a7b21a7a39ded6
audreyhasson <54117510+audreyhasson@users.noreply.github.com>
2023-09-16 12:45:46 -0400

    planet sliders

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -22,51 +22,112 @@ function getYPointOnEllipse(a, b, theta) {
   return res;
 }
 
+function distance(x0, y0, x1, y1) {
+  return ((x1-x0)**2 + (y1-y0)**2)**0.5
+}
+
 class Planet {
-  constructor(src, i, scale) {
+  constructor(src, i, scale = (1/15)) {
     this.src = src;
     this.travel = 0.1;
     this.i = i;
     this.scale = scale;
   }
 
+  getXY() {
+    const x = getXPointOnEllipse(getXRadius(this.i), getYRadius(this.i), Math.PI/2 * this.travel)
+    const y = getYPointOnEllipse(getXRadius(this.i), getYRadius(this.i), Math.PI/2 * this.travel) 
+    return x, y
+  }
+
   draw(context, canvas) {
     context.save();
 
-    const x = getXPointOnEllipse(getXRadius(this.i), getYRadius(this.i), Math.PI/2 * this.travel)//(Math.PI+ Math.PI) * this.travel );
-    const y = getYPointOnEllipse(getXRadius(this.i), getYRadius(this.i), Math.PI/2 * this.travel) // (Math.PI+ Math.PI) * this.travel);
+    const x = getXPointOnEllipse(getXRadius(this.i), getYRadius(this.i), Math.PI/2 * this.travel)
+    const y = getYPointOnEllipse(getXRadius(this.i), getYRadius(this.i), Math.PI/2 * this.travel) 
     
     const img = new Image();
     img.onload = () => {
-      context.drawImage(img, x+10, canvas.height-100-y, img.width/15, img.height/15);
+      context.drawImage(img, x+10, canvas.height-100-y, img.width*this.scale, img.height*this.scale);
     };
     img.src = this.src;
     context.restore();
   }
 }
 
-
-function draw() {
-  const canvas = document.getElementById("galaxyCanvas");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "rgba(255, 255, 255)";
-    for (let i = 0; i < 5; i++) {
-      ctx.strokeStyle = "rgba(255, 255, 255)";
-      ctx.beginPath();
-      // ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle)
-      ctx.ellipse(90, canvas.height-50, getXRadius(i), getYRadius(i), 0, 0, 2 * Math.PI);
-      ctx.stroke();
-    }
-    // Load and display planet
-    ctx.imageSmoothingEnabled = true;
-    for (let i = 0; i < 10; i++) {
-      const mars = new Planet(`/images/planet_${i+1}.png`, i);
-      mars.travel = 0.1*i;
-      mars.draw(ctx, canvas);
-    }
-    
+const planetList = [];
+for (let i = 0; i < 5; i++) {
+  let scale = 1/15;
+  if (i === 4) {
+    scale = 1/12;
   }
+  const mars = new Planet(`/images/planet_${i+1}.png`, i, scale=scale);
+  mars.travel = 0.1*i;
+  planetList.push(mars);
 }
-draw();
+
+
+// TRYING KONVA 
+
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+var stage = new Konva.Stage({
+  container: "galaxyCanvas",
+  width: width,
+  height: height,
+});
+
+
+var layer = new Konva.Layer();
+// add the layer to the stage
+stage.add(layer);
+
+
+for (let i = 0; i < 5; i++) {
+  var oval = new Konva.Ellipse({
+    x: 10,
+    y: stage.height(),
+    radiusX: getXRadius(i),
+    radiusY: getYRadius(i),
+    fill: '',
+    stroke: 'white',
+    strokeWidth: 4,
+  });
+  
+  // add the shape to the layer
+  layer.add(oval);
+}
+
+
+// add the layer to the stage
+
+
+for (let i = 0; i < 5; i++) {
+  var imageObj = new Image();
+  var planetLayer = new Konva.Layer();
+  imageObj.onload = function () {
+    var img = new Konva.Image({
+      image: imageObj,
+      x: getXPointOnEllipse(getXRadius(i), getYRadius(i), Math.PI /(2)) - 100,
+      y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(i), getYRadius(i),  Math.PI /2),
+      width: 200,
+      height: 137,
+      draggable: true,
+      dragBoundFunc: function (pos) {
+        let theta;
+        if (pos.x > getXRadius(i)) theta = 0;
+        else theta =  Math.PI/2 * (1 - pos.x/getXRadius(i));
+        return {
+          x: getXPointOnEllipse(getXRadius(i), getYRadius(i), theta) - 100,
+          y: stage.height() - 50 -  getYPointOnEllipse(getXRadius(i), getYRadius(i), theta),
+        };
+      },
+    });
+    planetLayer.add(img);
+    stage.add(planetLayer);
+  };
+  imageObj.src = `/images/planet_${i+1}.png`;
+  
+}
+
